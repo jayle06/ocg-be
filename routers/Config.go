5ed3bf9c4d994af57395d76eb53f6302a0119d7f
NamDuongkiwi <namduongkiwi@gmail.com>
2021-07-12 00:51:52 +0700

    update category api

--- routers/Config.go
+++ routers/Config.go
@@ -19,6 +19,7 @@ func RunServer() {
 	customerURL.HandleFunc("/products/{id}", controllers.GetProductByID).Methods("GET")
 	customerURL.HandleFunc("/new-products", controllers.GetNewProducts).Methods("GET")
 	customerURL.HandleFunc("/best-sale", controllers.GetBestSale).Methods("GET")
+	customerURL.HandleFunc("/categories", controllers.GetAllCategories).Methods("GET")
 
 	adminURL := r.PathPrefix("/admin").Subrouter()
 
@@ -35,6 +36,11 @@ func RunServer() {
 	adminURL.HandleFunc("/products/{id}", controllers.GetProductByID).Methods("GET")
 	adminURL.HandleFunc("/products/{id}", controllers.UpdateProductByID).Methods("PUT")
 	adminURL.HandleFunc("/products", controllers.CreateProduct).Methods("POST")
+
+	adminURL.HandleFunc("/categories", controllers.GetAllCategories).Methods("GET")
+	adminURL.HandleFunc("/categories", controllers.CreateCategory).Methods("POST")
+	adminURL.HandleFunc("/categories/{id}", controllers.UpdateCategory).Methods("PUT")
+	adminURL.HandleFunc("/categories/{id}", controllers.DeleteCategory).Methods("DELETE")
 
 	adminURL.HandleFunc("/uploads", utils.UploadFile).Methods("POST")
 	images := http.StripPrefix("/images/", http.FileServer(http.Dir("./uploads/")))
